59a69725d5df0db8d8efe66e4a0dc7ed4889bd39
NellySsenkumba <ssenkumbanelson.sn@gmail.com>
2025-02-04 10:31:16 +0300

    Added maven dependecy and updated to java 17

--- src/main/java/com/playground/SampleMediator.java
+++ src/main/java/com/playground/SampleMediator.java
@@ -1,22 +1,12 @@
 package com.playground;
 
-import org.apache.synapse.MessageContext; 
+import org.apache.synapse.MessageContext;
 import org.apache.synapse.mediators.AbstractMediator;
-    
-public class SampleMediator extends AbstractMediator { 
 
-    public boolean mediate(MessageContext context) { 
-        // TODO Implement your mediation logic here 
-        return true;
-    }
-}
+public class SampleMediator extends AbstractMediator {
 
-import org.apache.synapse.MessageContext; 
-import org.apache.synapse.mediators.AbstractMediator;
-    
-public class SampleMediator extends AbstractMediator { 
-
-    public boolean mediate(MessageContext context) { 
+    @Override
+    public boolean mediate(MessageContext context) {
         // TODO Implement your mediation logic here 
         return true;
     }
